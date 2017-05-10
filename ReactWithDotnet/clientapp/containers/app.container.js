0bd676c075236be0b63f692c02700698c8394294
Artur Ziemiański <aziemianski@hqsoftware.pl>
2017-05-10 21:36:55 +0200

    Base react structure

---
+++ ReactWithDotnet/clientapp/containers/app.container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AppContainer extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                Welcome on dotnetowo.pl
+            </div>
+        );
+    }
+}
+
+export default AppContainer;
---
+++ ReactWithDotnet/clientapp/root.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import AppContainer from './containers/app.container';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <AppContainer />,
+    document.getElementById('app-root')
+);
